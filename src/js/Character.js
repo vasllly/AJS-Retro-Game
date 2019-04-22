5efad227d6f54020502afd42a68b423e47ca8e91
Ganja Vasiliy <vasllly219@gmail.com>
2019-04-22 03:39:46 +0300

    17. Deployment

--- src/js/Character.js
+++
@@ -1,8 +0,0 @@
-export default class Character {
-  constructor(level) {
-    this.level = level;
-    this.health = 100;
-    // TODO: throw error if user use "new Character()"
-    if (new.target.name === 'Character') throw new Error('Нельзя создать объект класса Character!');
-  }
-}
